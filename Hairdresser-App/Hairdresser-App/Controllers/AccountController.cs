f5374d0313620b3f6b6901c3f39ddee4e821aa2a
erenkarasal <eren_karasal@hotmail.com>
2021-12-05 20:04:04 +0300

    Repo is updated

---
+++ Hairdresser-App/Hairdresser-App/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hairdresser_App.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("~/Account/Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [Route("~/Account/Signup")]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
