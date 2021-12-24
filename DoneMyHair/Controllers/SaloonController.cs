7968f4b5520b16c183469aeb60f025986081ccca
erenkarasal <eren_karasal@hotmail.com>
2021-12-24 21:10:40 +0300

    Yeni sayfalar eklendi

---
+++ DoneMyHair/Controllers/SaloonController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoneMyHair.Controllers
+{
+    public class SaloonController : Controller
+    {
+        // GET: Saloon
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+        public ActionResult Saloon()
+        {
+            return View();
+        }
+        public ActionResult Hairdresser()
+        {
+            return View();
+        }
+        public ActionResult NewHairdresser()
+        {
+            return View();
+        }
+        public ActionResult WaitingAppointments()
+        {
+            return View();
+        }
+    }
+}
